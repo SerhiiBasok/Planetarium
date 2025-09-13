3c1bec0de6c5dcea39ba36e3abff8c562ed07f49
Serhii Basok <sergeybsk19@gmail.com>
2025-09-13 14:15:55 +0300

    implement models to planetarium and migrations

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from planetarium.models import PlanetariumDome, AstronomyShow, ShowSession, ShowTheme, Reservation, Ticket
+
+admin.site.register(PlanetariumDome)
+admin.site.register(AstronomyShow)
+admin.site.register(ShowSession)
+admin.site.register(ShowTheme)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
+
+
+
--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,94 @@
 from django.db import models
+from core import settings
 
-# Create your models here.
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=63)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(
+        AstronomyShow,
+        on_delete=models.CASCADE,
+        related_name="show_sessions"
+    )
+    planetarium_dome = models.ForeignKey(
+        PlanetariumDome,
+        on_delete=models.CASCADE,
+        related_name="show_sessions"
+    )
+    show_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-show_time"]
+        unique_together = ["planetarium_dome", "show_time"]
+
+    def __str__(self):
+        return self.astronomy_show.title + " " + str(self.show_time)
+
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=63)
+    show = models.ManyToManyField(
+        AstronomyShow,
+        related_name="themes"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reservations"
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    show_session = models.ForeignKey(
+        ShowSession,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    def __str__(self):
+        return (
+            f"{str(self.show_session)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("show_session", "row", "seat")
+        ordering = ["row", "seat"]
