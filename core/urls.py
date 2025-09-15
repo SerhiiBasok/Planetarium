bcb4c70cca528c995e1796fccbab02a98836e601
Serhii Basok <sergeybsk19@gmail.com>
2025-09-15 21:46:57 +0300

    user logic and migratioms to user

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
-    # path("api/user/", include("user.urls", namespace="user")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
